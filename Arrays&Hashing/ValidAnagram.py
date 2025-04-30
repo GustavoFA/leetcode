223d26fd51b693909dfe3f7d4885bb579d359ae0
Gustavo Freitas <g236249@dac.unicamp.br>
2025-04-29 21:30:45 -0300

    Add hash function

--- Arrays&Hashing/ValidAnagram.py
+++ Arrays&Hashing/ValidAnagram.py
@@ -29,11 +29,29 @@ class ValidAnagram:
     def isAnagram_sorted(self) -> bool:
         """
             Using sort function.
+            
+            Time : O(nlogn + mlogm) | Space : O(1) or O(n+m)
         """
         if len(self.word_s) != len(self.word_t):
             return False
         
         return sorted(self.word_s) == sorted(self.word_t)
+    
+    def isAnagram_hash(self) -> bool:
+        """
+            Using hash maps (dicts).
+            
+            Time : O(n+m) | Space : O(1)
+        """
+        if len(self.word_s) != len(self.word_t):
+            return False
+        
+        countS, countT = {}, {}
+        
+        for i in range(len(self.word_s)):
+            countS[self.word_s[i]] = 1 + countS.get(self.word_s[i], 0)
+            countT[self.word_t[i]] = 1 + countT.get(self.word_t[i], 0)
+        return countS == countT
 
 if __name__ == "__main__":
    

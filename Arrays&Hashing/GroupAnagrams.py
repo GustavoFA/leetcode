68369797a30bf1d50c5bfecb85e313c58ffb0859
Gustavo Freitas <gprofi98@gmail.com>
2025-05-18 19:56:22 -0300

    Checkpoint

---
+++ Arrays&Hashing/GroupAnagrams.py
@@ -0,0 +1,12 @@
+class GroupAnagrams:
+    """
+        Given an array of strings strs, group all anagrams together into sublists. 
+        You may return the output in any order.
+    """
+
+    def __init__(self):
+        pass
+
+
+if __name__ == "__main__":
+    print()
